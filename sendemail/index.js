ccf7afeab75ef1506d298c3945c5623e7c916493
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-04 15:21:25 -0500

    email added

--- sendemail/index.js
+++ sendemail/index.js
@@ -1,33 +1,39 @@
 const nodemailer = require("nodemailer");
 
 module.exports = async function (context, req) {
+    const { DefaultAzureCredential } = require("@azure/identity");
+    const { SecretClient } = require("@azure/keyvault-secrets");
+    const credential = new DefaultAzureCredential();
+    const vaultName = "kaifamilyfarm3KV";
+    const url = `https://${vaultName}.vault.azure.net`;
+    const client = new SecretClient(url, credential);
+    
+    const userRetrievedSecret = await client.getSecret("username1");
+    const username1=userRetrievedSecret.value;
+    const pwdRetrievedSecret = await client.getSecret("password1");
+    const password1=pwdRetrievedSecret.value;
+
     let transporter = nodemailer.createTransport({
         service: "hotmail",
         auth: {
-            user: "kffsande12345@outlook.com",
-            pass: "Pwd4Kff5and3"
+          user: username1,
+          pass: password1,
         },
-        });
+    });
 
-        const mailOptions = {
-            from: 'kffsande12345@outlook.com',
-            to: 'kffsande12345@outlook.com',
-            subject:
-              'Order From ' + req.body.emailAddress + ' - ' + req.body.emailSubject,
-            text: req.body.emailBody,
-            html:
-              '<div><table><th><tr><th>Name</th><th>Quantity</th></tr></thead><tbody>' +
-              req.body.emailBody +
-              '<tr><td></td><td style="text-align:right; font-weight: bold;"><p></p>' +
-              req.body.orderTotal +
-              '</td></tr></tbody></table></div>',
-          };
+    const mailOptions = {
+        from: "kffsande12345@outlook.com",
+        to: "kffsande12345@outlook.com",
+        subject: "From vuenodemailer " + req.body.emailSubject,
+        text: req.body.emailBody,
+        html: '<div><table><thead><tr><th>Product ID</th><th>Name</th></tr></thead><tbody>' + req.body.emailBody + '<tr><td></td><td>$' + req.body.orderTotal + '</td></tr></tbody></table></div>',
+    }
 
-        transporter.sendMail(mailOptions,(error, info)=>{
-            if(error){
-                console.log(error);
-            }else{
-                console.log("Sent: " + info.response); 
-            }
-        }) 
-};
+    transporter.sendMail(mailOptions,(error, info)=>{
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Sent: " + info.response); 
+        }
+    }) 
+}
