97ba918f562bb5a4d740ac596747556bc39d39c4
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-04 14:25:50 -0500

    got email

--- sendemail/index.js
+++ sendemail/index.js
@@ -4,18 +4,24 @@ module.exports = async function (context, req) {
     let transporter = nodemailer.createTransport({
         service: "hotmail",
         auth: {
-            user: "kffsande@outlook.com",
+            user: "kffsande12345@outlook.com",
             pass: "Pwd4Kff5and3"
         },
         });
 
         const mailOptions = {
-            from: "kffsande@outlook.com",
-            to: "kffsande@outlook.com",
-            subject: req.body.emailSubject,
+            from: 'kffsande12345@outlook.com',
+            to: 'kffsande12345@outlook.com',
+            subject:
+              'Order From ' + req.body.emailAddress + ' - ' + req.body.emailSubject,
             text: req.body.emailBody,
-            html: '<div><table><thead><tr><th>Product ID</th><th>Name</th></tr></thead><tbody>' + req.body.emailBody + '</tbody></table></div>',
-        }
+            html:
+              '<div><table><th><tr><th>Name</th><th>Quantity</th></tr></thead><tbody>' +
+              req.body.emailBody +
+              '<tr><td></td><td style="text-align:right; font-weight: bold;"><p></p>' +
+              req.body.orderTotal +
+              '</td></tr></tbody></table></div>',
+          };
 
         transporter.sendMail(mailOptions,(error, info)=>{
             if(error){
